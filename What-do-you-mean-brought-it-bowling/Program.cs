34f579bea3e3e93501a7a2f361b865fe4977fa8b
Jack Serjeant <Jack.Serjeant@googlemail.com>
2015-04-18 03:09:07 +0100

    first project files

---
+++ What-do-you-mean-brought-it-bowling/Program.cs
@@ -0,0 +1,26 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using System.Linq;
+#endregion
+
+namespace What_do_you_mean_brought_it_bowling
+{
+#if WINDOWS || LINUX
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+#endif
+}
